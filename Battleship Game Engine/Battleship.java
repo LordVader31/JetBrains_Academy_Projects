5d0daf4e7089aa243296ea4febd210c0dab69396
Keane Moraes <lordvader3002@gmail.com>
2021-01-04 18:23:50 +0400

    UPDATE 2.1 - First Shots Working Model

--- Battleship Game Engine/Battleship.java
+++ Battleship Game Engine/Battleship.java
@@ -1,10 +1,9 @@
 package battleship;
 import java.util.Arrays;
 import java.util.Scanner;
-public class Battleship {
+public class Main {
 
     static char[][] battlefield = new char[10][10];
-    static String[] occupied = new String[5];
     static final String[] ships = {"Aircraft Carrier", "Battleship", "Submarine", "Cruiser", "Destroyer"};
     static final int[] shipLengths = {5, 4, 3, 3, 2};
 
@@ -18,6 +17,9 @@ public class Battleship {
             placeShips(i);
             printBattlefield();
         }
+        System.out.println("\nThe game starts!\n");
+        printBattlefield();
+        firingASalvo();
     }
 
     private static void printBattlefield() {
@@ -33,6 +35,7 @@ public class Battleship {
             }
             row++;
         }
+        System.out.println();
     }
 
     private static void placeShips(int pos) {
@@ -49,83 +52,140 @@ public class Battleship {
             int columnOfFirst = Integer.parseInt(firstCoordinate.substring(1));
             int columnOfSecond = Integer.parseInt(secondCoordinate.substring(1));
 
-            // CHECK FOR COORDINATES OUTSIDE THE BOARD
-            if (rowOfFirst > 'J' || rowOfFirst < 'A' || rowOfSecond > 'J' || rowOfSecond < 'A') {
-                System.out.print("\nError! Invalid Row Coordinates. Please enter a value between A and J.");
-                System.out.println(" Try again :");
+            if (!isCorrectCoordinates(rowOfFirst, rowOfSecond, columnOfFirst, columnOfSecond, pos))
                 continue;
-            } else if (columnOfFirst > 10 || columnOfFirst < 1 || columnOfSecond > 10 || columnOfSecond < 1) {
-                System.out.print("\nError! Invalid Column Coordinates. Please enter a value between 1 and 10.");
-                System.out.println(" Try again :");
-                continue;
-            }
 
-            // CHECK FOR COORDINATES NOT CORRESPONDING TO STRAIGHT LINES
-            if (rowOfFirst != rowOfSecond && columnOfFirst != columnOfSecond) {
-                System.out.println("Error! Wrong ship location! Try again:");
-                continue;
-            } else if (rowOfFirst == rowOfSecond) {
-                if (Math.abs(columnOfFirst - columnOfSecond) + 1 != shipLength) {
-                    System.out.println("Error! Wrong length (column ex) of the " + shipType +"! Try again:");
-                    continue;
-                }
-            } else {
-                if (Math.abs(rowOfFirst - rowOfSecond) + 1 != shipLength) {
-                    System.out.println("Error! Wrong length (row ex) of the " + shipType +"! Try again:");
-                    continue;
-                }
-            }
+            // NORMALIZE THE ROW AND COLUMN COORDINATES
+            int temp = Math.max(columnOfFirst, columnOfSecond);
+            columnOfFirst = Math.min(columnOfFirst, columnOfSecond);
+            columnOfSecond = temp;
+
+            temp = rowOfFirst > rowOfSecond ? rowOfFirst : rowOfSecond;
+            rowOfFirst = rowOfFirst < rowOfSecond ? rowOfFirst : rowOfSecond;
+            rowOfSecond = (char) temp;
 
             // CHECK FOR CROSSING OR TOUCHING OTHER SHIPS
-            boolean crossOrTouch = false;
-                // CHECK FOR CROSSING OTHER SHIPS
-                for (int i = rowOfFirst - 65; i <= rowOfSecond - 65; i++) {
-                    for (int j = columnOfFirst - 1; j < columnOfSecond; j++) {
-                        if (battlefield[i][j] == '0') {
-                            crossOrTouch = true;
-                            System.out.println("Error! Your ships cannot cross one another. Try again:");
-                            break;
-                        }
-                    }
-                    if (crossOrTouch) break;
+            boolean cross = isCrossing(rowOfFirst, rowOfSecond, columnOfFirst, columnOfSecond);
+            boolean touch = isTouching(rowOfFirst, rowOfSecond, columnOfFirst, columnOfSecond);
+            if (cross || touch) continue;
+
+            for (int i = rowOfFirst - 65; i <= rowOfSecond - 65; i++) {
+                for (int j = columnOfFirst - 1; j < columnOfSecond; j++) {
+                    battlefield[i][j] = 'O';
                 }
-
-                //CHECK FOR TOUCHING OTHER SHIPS
-                int n = pos;
-                while (occupied[n] != null) {
-                    String firstCoordShip = occupied[n].substring(0, occupied[n].indexOf(':'));
-                    String secondCoordShip = occupied[n].substring(occupied[n].indexOf(':')+1);
-                    char rowOfFPS = firstCoordinate.charAt(0);
-                    char rowOfLPS = secondCoordinate.charAt(0);
-                    int columnOfFPS = Integer.parseInt(firstCoordinate.substring(1));
-                    int columnOfLPS = Integer.parseInt(secondCoordinate.substring(1));
-
-
-                }
-
-            if (!crossOrTouch)  {
-
-                // NORMALIZE THE ROW AND COLUMN COORDINATES
-                int temp = Math.max(columnOfFirst, columnOfSecond);
-                columnOfFirst = Math.min(columnOfFirst, columnOfSecond);
-                columnOfSecond = temp;
-
-                temp = rowOfFirst > rowOfSecond ? rowOfFirst : rowOfSecond;
-                rowOfFirst = rowOfFirst < rowOfSecond ? rowOfFirst : rowOfSecond;
-                rowOfSecond = (char) temp;
-
-                for (int i = rowOfFirst - 65; i <= rowOfSecond - 65; i++) {
-                    for (int j = columnOfFirst - 1; j < columnOfSecond; j++) {
-                        battlefield[i][j] = 'O';
-                    }
-                }
-                occupied[pos] = firstCoordinate + ":" + secondCoordinate;
-                break;
             }
-
+            break;
+        } 
+    }
+    
+    private static boolean isCorrectCoordinates(char roF, char roS, int coF, int coS, int pos) {
+        
+        // CHECK FOR COORDINATES OUTSIDE THE BOARD
+        if (roF > 'J' || roF < 'A' || roS > 'J' || roS < 'A') {
+            System.out.print("\nError! Invalid Row Coordinates. Please enter a value between A and J.");
+            System.out.println(" Try again :");
+            return false;
+        } else if (coF > 10 || coF < 1 || coS > 10 || coS < 1) {
+            System.out.print("\nError! Invalid Column Coordinates. Please enter a value between 1 and 10.");
+            System.out.println(" Try again :");
+            return false;
         }
 
+        if (pos != -1) {
+            // CHECK FOR COORDINATES NOT CORRESPONDING TO STRAIGHT LINES
+            if (roF != roS && coF != coS) {
+                System.out.println("Error! Wrong ship location! Try again:");
+                return false;
+            } else if (roF == roS) {
+                if (Math.abs(coF - coS) + 1 != shipLengths[pos]) {
+                    System.out.println("Error! Wrong length of the " + ships[pos] + "! Try again:");
+                    return false;
+                }
+            } else {
+                if (Math.abs(roF - roS) + 1 != shipLengths[pos]) {
+                    System.out.println("Error! Wrong length of the " + ships[pos] + "! Try again:");
+                    return false;
+                }
+            }
+        }
 
-    }//end of String decrypt(String)
+        return true;
+    }
+    
+    private static boolean isCrossing(char roF, char roS, int coF, int coS) {
+        // CHECK FOR CROSSING OTHER SHIPS OR TOUCHING OTHER SHIPS
+        for (int i = roF - 65; i <= roS - 65; i++) {
+            for (int j = coF - 1; j <= coS - 1; j++) {
+                if (battlefield[i][j] == '0') {
+                    System.out.println("Error! Your ships cannot cross one another. Try again:");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
+    private static boolean isTouching(char roF, char roS, int coF, int coS) {
+        // CHECK FOR CROSSING OTHER SHIPS OR TOUCHING OTHER SHIPS
+        boolean touch = false;
+        for (int i = roF - 65; i <= roS - 65; i++) {
+            for (int j = coF - 1; j <= coS - 1; j++) {
+                if (roF == roS) {
+                    if (coF - 2 >= 0)
+                        touch = battlefield[roF - 65][coF - 2] == 'O';
+                    if (coS <= 9)
+                        touch = battlefield[roF - 65][coS] == 'O' || touch;
+
+                    if (roF - 66 >= 0)
+                        touch = battlefield[roF - 66][j] == 'O' || touch;
+                    if (roS - 64 <= 9)
+                        touch = battlefield[roS - 64][j] == 'O' || touch;
+                } else {
+                    if (roF - 66 >= 0)
+                        touch = battlefield[roF - 66][coF - 1] == 'O';
+                    if (roS - 64 <= 9)
+                        touch = battlefield[roS - 64][coF - 1] == 'O' || touch;
+
+                    if (coF - 2 >= 0)
+                        touch = battlefield[i][coF - 2] == 'O' || touch;
+                    if (coS <= 9)
+                        touch = battlefield[i][coS] == 'O' || touch;
+                }
+                if (touch) {
+                    System.out.println("Error! You placed it too close to another one. Try again:");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private static void firingASalvo() {
+        Scanner num = new Scanner(System.in);
+        System.out.println("\nTake a shot!");
+        while (true) {
+            String firingPos = num.next().toUpperCase().trim();
+
+            char rowCoordinate = firingPos.charAt(0);
+            int columnCoordinate = Integer.parseInt(firingPos.substring(1));
+
+            if (!isCorrectCoordinates(rowCoordinate, 'A', columnCoordinate, 9, -1)) {
+                System.out.println("Error! You entered the wrong coordinates! Try again:");
+                continue;
+            }
+
+            if (battlefield[rowCoordinate - 65][columnCoordinate - 1] == 'O') {
+                battlefield[rowCoordinate - 65][columnCoordinate - 1] = 'X';
+                printBattlefield();
+                System.out.println("You hit a ship!");
+
+            }
+            else if (battlefield[rowCoordinate - 65][columnCoordinate -1] == '~') {
+                battlefield[rowCoordinate - 65][columnCoordinate - 1] = 'M';
+                printBattlefield();
+                System.out.println("You missed!");
+            }
+            break;
+        }
+    }
 }

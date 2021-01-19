16519b6b218bca3f7743622fa1a2b6160e03bc22
Keane Moraes <lordvader3002@gmail.com>
2021-01-19 21:22:02 +0400

    U2.12 - Engine Development

--- Tic-Tac-Toe with AI/Players/Engine.java
+++ Tic-Tac-Toe with AI/Players/Engine.java
@@ -1,3 +1,21 @@
-public abstract class Engine {
+package TicTacToe.Players;
 
+import TicTacToe.Board;
+
+abstract class Engine {
+    protected char[][] board;
+
+    public void reconfigureBoard(Board b) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (Board.board[i][j] == 'X')
+                    board[i][j] = 'X';
+                else if (Board.board[i][j] == 'O')
+                    board[i][j] = 'O';
+                else board[i][j] = ' ';
+            }
+        }
+    }
+
+    public abstract Board makeAMove();
 }

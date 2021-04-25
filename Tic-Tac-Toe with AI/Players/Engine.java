4a840e80b99bdb8e50e0100c2312bab5594d98e1
Keane Moraes <lordvader3002@gmail.com>
2021-04-25 19:04:09 +0400

    U5.2.2 - Engine S2

--- Tic-Tac-Toe with AI/Players/Engine.java
+++ Tic-Tac-Toe with AI/Players/Engine.java
@@ -1,21 +1,7 @@
-package TicTacToe.Players;
-
-import TicTacToe.Board;
+package tictactoe.Players;
+import tictactoe.Board;
 
 abstract class Engine {
-    protected char[][] board;
 
-    public void reconfigureBoard(Board b) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (Board.board[i][j] == 'X')
-                    board[i][j] = 'X';
-                else if (Board.board[i][j] == 'O')
-                    board[i][j] = 'O';
-                else board[i][j] = ' ';
-            }
-        }
-    }
-
-    public abstract Board makeAMove();
+    public abstract void makeAMove(Board game);
 }

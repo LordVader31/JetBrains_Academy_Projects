e7abd593f1c2cc4035d3bf1bd4cebf60b7f4df0c
Keane Moraes <lordvader3002@gmail.com>
2021-04-26 12:19:16 +0400

    U5.7.3 - Medium Engine Refactor

--- Tic-Tac-Toe with AI/Players/Medium.java
+++ Tic-Tac-Toe with AI/Players/Medium.java
@@ -1,2 +1,28 @@
-public class Medium extends Engine {
-}
+package tictactoe.Players.Engines;
+
+import tictactoe.Players.Engines.Engine;
+import tictactoe.Board;
+
+final public class Medium extends Engine {
+    public Medium(char playerType) {
+        this.playerType = playerType;
+    }
+
+    public void setPlayerType(char playerType) {
+        this.playerType = playerType;
+    }
+
+    public char getPlayerType() {
+        return playerType;
+    }
+
+    @Override
+    public void makeAMove(Board game) {
+
+    }
+
+    private int[] isOpponentWinning(Board game, char opponentType) {
+
+        return new int[]{0, 0};
+    }
+}//end of class

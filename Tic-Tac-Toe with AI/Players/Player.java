fd5e6ebb0d7482af040518cd4d60c527114d9771
Keane Moraes <lordvader3002@gmail.com>
2021-04-27 13:05:19 +0400

    U5.11.1 - S3 Player Documentation

--- Tic-Tac-Toe with AI/Players/Player.java
+++ Tic-Tac-Toe with AI/Players/Player.java
@@ -5,5 +5,11 @@ import tictactoe.Board;
 public abstract class Player {
     public char playerType;
 
+    /**
+     * makeAMove(Board)
+     * consumes a Board and makes a move on the board and displays it. The move depends
+     * whether the player is an EASY_AI, MEDIUM_AI, HARD_AI or HUMAN
+     * @param game - the current board state
+     */
     public abstract void makeAMove(Board game);
 }//end of class

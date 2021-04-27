1441565bc5408a38d5ab461a28500b594618260d
Keane Moraes <lordvader3002@gmail.com>
2021-04-27 13:07:33 +0400

    U5.11.3  - Engine design changes

--- Tic-Tac-Toe with AI/Players/Engines/Engine.java
+++ Tic-Tac-Toe with AI/Players/Engines/Engine.java
@@ -1,16 +1,27 @@
 package tictactoe.Players.Engines;
-import tictactoe.Board;
 import tictactoe.Players.Player;
 import java.util.Random;
-
+import tictactoe.Board;
 public abstract class Engine extends Player {
 
-    protected int[] generateRandomCoordinates() {
+    private int[] generateRandomCoordinates() {
         Random random = new Random();
         int[] randomCoord = new int[2];
-        randomCoord[0] = random.nextInt(9) / 3;
-        randomCoord[1] = random.nextInt(9) % 3;
+        randomCoord[0] = random.nextInt(9) / 3 + 1;
+        randomCoord[1] = random.nextInt(9) % 3 + 1;
 
         return randomCoord;
+    }
+    
+    protected void placePieceRandomly(Board game) {
+        int[] randomCoord = generateRandomCoordinates();
+        int row = randomCoord[0] - 1;
+        int column = randomCoord[1] - 1;
+        while (game.board[row][column] == 'X' || game.board[row][column] == 'O') {
+            randomCoord = generateRandomCoordinates();
+            row = randomCoord[0] - 1 ;
+            column = randomCoord[1] - 1;
+        }
+        game.board[row][column] = playerType;
     }
 }

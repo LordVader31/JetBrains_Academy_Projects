497cfeac038ddc7752ee71e9643631c1505b2686
Keane Moraes <lordvader3002@gmail.com>
2021-04-30 12:59:33 +0400

    U5.20.2 - Readability S2 sentences

--- Readability Score/Main.java
+++ Readability Score/Main.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner txt = new Scanner(System.in);
+        final String sentenceSplitRegex = "[.!?]";
         String input = txt.nextLine();
-        if (input.length() <= 100) {
+
+        String[] sentences = input.split(sentenceSplitRegex);
+        int sum = 0;
+        for (String sentence : sentences) {
+            sum += countWordsInSentence(sentence.trim());
+        }
+
+        final double average = ((double) sum) / sentences.length;
+        if (average <= 10d) {
             System.out.println("EASY");
         } else {
             System.out.println("HARD");
         }
+    }
+
+    private static int countWordsInSentence(String sentence) {
+        return sentence.length() - sentence.replaceAll(" ", "").length() + 1;
     }
 }

a69af260d7e4e6216c7d3eb599b86ce5e31ff6cb
Keane Moraes <lordvader3002@gmail.com>
2021-05-01 14:58:24 +0400

    U6.4 - Main design changes

--- Readability Score/Main.java
+++ Readability Score/Main.java
@@ -1,6 +1,5 @@
 package readability;
-import readability.Indexes.Automated;
-import readability.Indexes.ReadabilityTest;
+import readability.Indexes.TextInformation;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -28,12 +27,10 @@ public class Main {
         System.out.print("Enter the score you want to calculate (ARI, FK, SMOG, CL, all): ");
         char option = txt.next().trim().charAt(0);
 
+        TextInformation info = new TextInformation(data.toString());
         double averageAge = 0d;
-        ReadabilityTest test1;
         switch (option) {
             case 'A' :
-                test1 = new Automated(data.toString());
-                test1.printData();
 
                 break;
 
@@ -50,46 +47,5 @@ public class Main {
                 break;
         }
         System.out.printf("This text should be understood on average by %.2f-year-olds.", averageAge);
-    }
-
-    public static void determineReadingLevel(String input) {
-        final String sentenceSplitRegex = "[.!?]";
-        String[] sentenceArray = input.split(sentenceSplitRegex);
-        final int SENTENCES = sentenceArray.length;
-        final int CHARACTERS = input.replaceAll("\\s", "").length();
-        int WORDS = 0;
-        for (String sentence : sentenceArray) {
-            String[] wordArray = sentence.trim().split(" ");
-            WORDS += wordArray.length;
-        }
-
-        final double SCORE = 4.71 * (1.0 * CHARACTERS / WORDS) + 0.5 * (1.0 * WORDS / SENTENCES) - 21.43;
-
-        System.out.printf("The text is:\n%s\n", input);
-
-        System.out.printf("Words: %d\n" +
-                        "Sentences: %d\n" +
-                        "Characters: %d\n" +
-                        "The score is: %.2f\n" +
-                        "This text should be understood by %S-year-olds.",
-                WORDS, SENTENCES, CHARACTERS, SCORE, getAgeRange(SCORE));
-    }
-
-    public static String getAgeRange(double score) {
-        int scoreAsInt = (int) Math.ceil(score);
-
-        if (scoreAsInt > 14) {
-            return "0";
-        } else if (scoreAsInt == 3) {
-            return "7-9";
-        } else if (scoreAsInt == 13) {
-            return "18-24";
-        } else if (scoreAsInt == 14) {
-            return "24+";
-        } else if (scoreAsInt < 3){
-            return (scoreAsInt + 4) + "-" + (scoreAsInt + 5);
-        } else {
-            return (scoreAsInt + 5) + "-" + (scoreAsInt + 6);
-        }
     }
 }

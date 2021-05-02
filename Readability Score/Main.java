3226ac4431e6c633f9798562d5260f07b1536459
Keane Moraes <lordvader3002@gmail.com>
2021-05-02 21:11:28 +0400

    U6.5 - Implementaion changes

--- Readability Score/Main.java
+++ Readability Score/Main.java
@@ -1,5 +1,5 @@
 package readability;
-import readability.Indexes.TextInformation;
+import readability.Indexes.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,7 +13,7 @@ public class Main {
         }
 
         // READING IN THE DATA
-        StringBuilder data = new StringBuilder();
+        StringBuilder data = new StringBuilder(txt.nextLine());
         File file = new File(args[0]);
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
@@ -28,23 +28,38 @@ public class Main {
         char option = txt.next().trim().charAt(0);
 
         TextInformation info = new TextInformation(data.toString());
+        info.printData();
         double averageAge = 0d;
+        Automated testAuto = new Automated();
+        FleschKincaid testFK = new FleschKincaid();
+        ColemanLiau testCL = new ColemanLiau();
+        SMOG testSM = new SMOG();
         switch (option) {
             case 'A' :
-
+                testAuto.determineReadingLevel(info);
                 break;
 
             case 'F':
+                testFK.determineReadingLevel(info);
                 break;
 
             case 'S':
+                testSM.determineReadingLevel(info);
                 break;
 
             case 'C':
+                testCL.determineReadingLevel(info);
                 break;
 
             case 'a' :
+                testAuto.determineReadingLevel(info);
+                testFK.determineReadingLevel(info);
+                testSM.determineReadingLevel(info);
+                testCL.determineReadingLevel(info);
                 break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + option);
         }
         System.out.printf("This text should be understood on average by %.2f-year-olds.", averageAge);
     }
